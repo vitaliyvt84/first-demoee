3f90451a7c0917de0b5ca36fa1a429d89540fa53
vitaliytv <vitaliy.vt84@gmail.com>
2016-10-06 20:25:46 +0300

    Initial commit

---
+++ src/main/java/org/demo/Main.java
@@ -0,0 +1,14 @@
+package org.demo;
+
+public class Main {
+    private String a;
+
+    private void m(int i) {
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
